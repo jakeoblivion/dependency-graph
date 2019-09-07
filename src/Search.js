8e2d5a41b9f900c59e1dab7773725b9aa31d1286
jakeoblivion <jakeoblivion@gmail.com>
2019-09-07 21:40:50 +0100

    Visualise immediate dependencies on a graph

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import { get } from 'lodash'
+import { get, isEmpty } from 'lodash'
+import { ForceGraph2D } from 'react-force-graph';
 
 const SearchSearch = () => {
+  const [mainPackageName, setMainPackageName] = useState();
+  const [mainPackageVersion, setMainPackageVersion] = useState();
+  const [mainPackageDescription, setMainPackageDescription] = useState();
   const [data, setData] = useState();
   const [query, setQuery] = useState();
 
@@ -12,9 +16,20 @@ const SearchSearch = () => {
     async function fetchData() {
       const response = await axios.get('https://registry.npmjs.cf/' + query).then((response) => response.data);
       if (!ignore) {
-          const latestVersionNumber = Object.keys(response.versions).length-1
-          const latestVersion =  Object.keys(response.versions)[latestVersionNumber]
-          setData(JSON.stringify(response.versions[latestVersion].dependencies))
+        const latestVersion = get(response, 'dist-tags.latest')
+        const packageName = get(response, 'name')
+        setMainPackageVersion(latestVersion)
+        setMainPackageName(packageName)
+        setMainPackageDescription(get(response,'description'))
+        const packageNode = {id: packageName, name: packageName, val: 3}
+
+        const dependencies = get(response, 'versions')[latestVersion].dependencies
+        if(isEmpty(dependencies)) return
+        const dependencyNodes = Object.entries(dependencies).map(dependency => ({id: dependency[0], name: dependency[0]}))
+        const dependencyLinks = Object.entries(dependencies).map(dependency =>({source: packageName, target: dependency[0]}))
+
+        dependencyNodes.push(packageNode)
+        setData({nodes: dependencyNodes, links: dependencyLinks})
       }
     }
 
@@ -24,8 +39,28 @@ const SearchSearch = () => {
 
   return (
     <>
-      <input value={query} onChange={e => setQuery(e.target.value)} />
-      {data}
+      <input className="search" value={query} onChange={e => setQuery(e.target.value)} />
+      <h2>{mainPackageName} {mainPackageVersion}</h2>
+      <h4>{mainPackageDescription}</h4>
+
+      {!isEmpty(data,'nodes') && <ForceGraph2D
+            graphData={data}
+            nodeAutoColorBy="group"
+            nodeCanvasObject={(node, ctx, globalScale) => {
+              const label = node.id;
+              const fontSize = 12/globalScale;
+              ctx.font = `${fontSize}px Sans-Serif`;
+              const textWidth = ctx.measureText(label).width;
+              const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+              ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+              // ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+              ctx.textAlign = 'center';
+              ctx.textBaseline = 'middle';
+              ctx.fillStyle = node.color;
+              ctx.fillText(label, node.x, node.y);
+            }}
+          />
+        }
     </>
   );
 }
